a4a3c4de75d37f067b5dfb24b0abe6ce605859f4
Freddy Song <fredsong99@gmail.com>
2025-01-10 20:51:56 -0800

    feat: listener, scripts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,33 @@
 import app from './app';
+import clc from 'cli-color';
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || '3000');
 
-await app.listen({ port: Number(PORT), host: '0.0.0.0' }, (err, address) => {
-  if (err) {
-    console.error(err);
+const asciiArt = `
+░▒▓██████▓▒░  
+░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░                Welcome to cafeXpress
+░▒▓█▓▒░                
+░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░ 
+ ░▒▓██████▓▒░  
+  `;
+
+const start = async () => {
+  try {
+    await app.listen({
+      port: PORT,
+      host: '0.0.0.0'
+    });
+    console.log(`
+        ${clc.blueBright(asciiArt)}
+      Address: ${app.addresses()[0].address}
+      Port: ${app.addresses()[0].port}
+      `);
+  } catch (error) {
+    console.log(error);
     process.exit(1);
   }
-  console.log(`Server running at ${address}`);
-});
+};
+
+start();
