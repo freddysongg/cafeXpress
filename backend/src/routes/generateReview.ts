b4669e0785800fec38b28e3a8d78ad8729542c65
Alex Yen <alexanderyen88@gmail.com>
2025-03-21 03:21:52 -0700

    generatereview

--- backend/src/routes/generateReview.ts
+++ backend/src/routes/generateReview.ts
@@ -6,17 +6,16 @@ import { generateReviewsForCafe } from '@services/generateReview.js';
 export async function generateRoutes(fastify: FastifyInstance) {
   fastify.post('/', async (request, reply) => {
     try {
-      //const allCafes = await db.select().from(cafes);
-      const firstCafe = await db.select().from(cafes).limit(1); // Get only the first cafe
+      const allCafes = await db.select().from(cafes);
 
-      // if (allCafes.length === 0) {
-      //   return reply.status(404).send({ message: 'No cafes found in the database.' });
-      // }
+      if (allCafes.length === 0) {
+        return reply.status(404).send({ message: 'No cafes found in the database.' });
+      }
 
-      // for (const cafe of allCafes) {
-      //   await generateReviewsForCafe(cafe);
-      // }
-      await generateReviewsForCafe(firstCafe[0]);
+      for (const cafe of allCafes) {
+        await generateReviewsForCafe(cafe);
+      }
+      //await generateReviewsForCafe(firstCafe[0]);
 
       return reply.status(200).send({ message: 'Fake reviews generated for all cafes.' });
     } catch (error) {
